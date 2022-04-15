2a38bf9b9118e4143c45578e98062d9340707762
Lelio Victor <leliov.vieira@gmail.com>
2022-04-14 23:50:56 -0300

    Initial layout done with 3 static message to style css

---
+++ scripts.js
@@ -0,0 +1,5 @@
+//MENSAGEM: 
+
+//`<div class="login"><h1><span class="time">(${TEMPO})</span>  <span class="person">${FALANTE}</span> entra na sala...</h1></div>`
+//`<div class="reserved"><h1><span class="time">(${TEMPO})</span>  <span class="person">${FALANTE}</span> reservadamente para <span class="person">${RECEBIDO}</span>: ${MENSAGEM}</h1></div>`
+//`<div class="message"><h1><span class="time">(${TEMPO})</span>  <span class="person">${FALANTE}</span> para <span class="person">${RECEBIDO}</span>:  ${MENSAGEM}</h1></div>
