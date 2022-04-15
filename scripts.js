14e1c9f7e530a02ef72659d1638e1643cf49e14a
Lelio Victor <leliov.vieira@gmail.com>
2022-04-15 16:24:49 -0300

    Get Messages from API and render these message done

--- scripts.js
+++ scripts.js
@@ -1,28 +1,32 @@
 const user = 'luffy'; //COM PROMPT E DEPOIS FAZER EM OUTRA TELA (BONUS)
 const userObject = {name: `${user}`};
+
+//REMOVER O DEU ERRADO E DEU CERTO DE FUNCAO E DENTRO DOS CATCH NO FINAL DO PROJETO;
+//REMOVER FUNCOES DE TESTE, FINAL DO JS
+
+
 //MENSAGEM: 
 
 //`<div class="login"><h1><span class="time">(${TEMPO})</span>  <span class="person">${FALANTE}</span> entra na sala...</h1></div>`
 //`<div class="reserved"><h1><span class="time">(${TEMPO})</span>  <span class="person">${FALANTE}</span> reservadamente para <span class="person">${RECEBIDO}</span>: ${MENSAGEM}</h1></div>`
 //`<div class="message"><h1><span class="time">(${TEMPO})</span>  <span class="person">${FALANTE}</span> para <span class="person">${RECEBIDO}</span>:  ${MENSAGEM}</h1></div>
 
+//3 tipos de mensagem - status, message, private_message
+
+
 function login() {
     const promisse = axios.post("https://mock-api.driven.com.br/api/v6/uol/participants",userObject);
 
-    promisse.then(renderLogin);
+    promisse.then(startChat);
     promisse.catch(deuErrado);
 
 }
 
-function renderLogin() {
-    const element = document.querySelector("main");
-    element.innerHTML += `
-    <div class="status">
-    <p><span class="time">(${getTime()})</span> <span class="person">${user}</span> entra na sala...</p>
-    </div>
-    `
-
+function startChat() {
     setInterval(refreshLogin,5000);
+
+    getAllMessages();
+    setInterval(getAllMessages,3000);
 }
 
 function refreshLogin() {
@@ -32,6 +36,58 @@ function refreshLogin() {
 
 }
 
+function getAllMessages() {
+    const promisse = axios.get("https://mock-api.driven.com.br/api/v6/uol/messages");
+
+    promisse.then(getMessage);
+    promisse.catch(deuErrado);
+}
+
+function getMessage(arrObject) {
+    cleanLastMessages();
+
+    const messages = arrObject.data;
+    messages.map(renderMessage);
+}
+
+function cleanLastMessages() {
+    document.querySelector("main").innerHTML = "";
+}
+
+function renderMessage(object) {
+    const element = document.querySelector("main");
+    
+    if (object.type === 'status') {
+        element.innerHTML += `
+        <div class="status">
+        <p><span class="time">(${object.time})</span> <span class="person">${object.from}</span> ${object.text}</p>
+        </div>
+        `
+    }
+
+    if(object.type === 'message') {
+        element.innerHTML += `
+        <div class="message">
+        <p><span class="time">(${object.time})</span> <span class="person">${object.from}</span> para <span class="person">${object.to}</span>: ${object.text}</p>
+        </div>
+        `
+    }
+    
+    if(object.type === 'private_message') {
+        element.innerHTML += `
+        <div class="private_message">
+        <p><span class="time">(${object.time})</span> <span class="person">${object.from}</span> reservadamente para <span class="person">${object.to}</span>: ${object.text}</p>
+        </div>
+        `
+    }
+
+    LastMessageIntoView();
+}
+
+function LastMessageIntoView() {
+    const lastMessage = document.querySelector("main div:last-child");
+    lastMessage.scrollIntoView();
+}
 
 function sendMessage() {
     const message = document.querySelector("input").value;
@@ -48,6 +104,19 @@ function sendMessage() {
     promisse.catch(deuErrado);
 }
 
+function getTime() {
+    const date = new Date();
+    const timer = `${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`
+    return timer;
+}
+
+
+
+
+//FUNCOES DE TESTE, REMOVER AO FIM DO PROJETO
+
+
+
 function deuCerto() {
     console.log("ok");
 }
@@ -56,10 +125,6 @@ function deuErrado() {
     alert('deu ruim');
 }
 
-function getTime() {
-    const date = new Date();
-    const timer = `${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`
-    return timer;
+function mostrar(objeto) {
+    console.log(objeto);
 }
-
-//3 tipos de mensagem - status, message, private_message
